89dff68c4a56c9feb0f59439c4395104912a48f8
maximo santoro <maximo.santoro@iovlabs.org>
2022-12-05 16:29:00 -0300

    add function test

--- nu-hardhat-lab/test/cars-state.spec.js
+++ nu-hardhat-lab/test/cars-state.spec.js
@@ -20,6 +20,7 @@ describe('Cars - state machine', () => {
     });
 
     it('should add a new car', async () => {
+        // static call
         const returnVal = 
             await cars
                 .connect(accounts[1])
@@ -32,5 +33,29 @@ describe('Cars - state machine', () => {
                     },
                 );
         expect(returnVal).to.equal(1);  
+        
+        // simulate state trans (tx)
+        const tx = 
+            await cars
+                .connect(accounts[1])
+                .functions
+                .addCar(
+                    '0xff00ff', //color
+                    4, // doors
+                    {
+                        value: ethers.utils.parseEther("1.1"), // 1.1 RBTC
+                    },
+                );
+        const numCars = await cars.callStatic.numCars();
+        const car1 = await cars.callStatic.cars(1);
+        //console.log(car1);
+
+        expect(numCars).to.equal(1);
+        expect(car1.colour).to.equal('0xff00ff');
+        expect(car1.doors).to.equal(4);
+        expect(car1.status).to.equal(1);
+
+        expect(car1.owner).to.equal(accounts[1].address);
+
     });
 });
